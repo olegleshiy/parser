229082cad0a55ba71bd4950503ce3fcbb4ca4049
Oleh Zahrebelnyi <o.zahrebelnyi@bit-flow.io>
2020-08-06 18:27:45 +0300

    changed query

--- parser.js
+++ parser.js
@@ -27,32 +27,14 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get('/', async (req, res) => {
     try {
-        const query = "SELECT publish_date FROM posts WHERE resource = 'news' GROUP BY DESC LIMIT 1;";
+        const query = "SELECT publish_date FROM posts WHERE resource = 'news' ORDER BY publish_date ASC LIMIT 1;";
         conn.query(query, function (error, data, fields) {
             if (error) throw error;
 
             console.log('newPost2', data);
+            res.send(`<h1>${data[0].publish_date}</h1>`)
         });
-        //const result = await getFinancePost(conn);
-        // https.get('https://finance.yahoo.com/rss/', (res) => {
-        //     console.log('statusCode:', res.statusCode);
-        //     console.log('headers:', res.headers);
-        //     let data = '';
-        //     res.on('data', (d) => {
-        //         data += d;
-        //     });
-        //     res.on('end', () => {
-        //         fs.writeFile('index.xml', data, (err) => {
-        //             if (err) throw err;
-        //             console.log('The file has been saved!');
-        //         });
-        //     });
-        //
-        // }).on('error', (e) => {
-        //     console.error(e);
-        // });
 
-        res.send('<h1>Hello</h1>')
     } catch (e) {
         console.log(e);
     }
@@ -77,11 +59,10 @@ app.get('/news', async (req, res) => {
 
 app.get('/entertainment', async (req, res) => {
     try {
-        const query = 'SELECT * FROM posts WHERE resource = "entertainment" ORDER BY publish_date DESC LIMIT 5;';
+        const query = 'SELECT * FROM posts WHERE resource = "entertainment" ORDER BY publish_date DESC LIMIT 10;';
         conn.query(query, function (error, data, fields) {
             if (error) throw error;
 
-            //const results = data.sort((a, b) => a.publish_date > b.publish_date ? 1 : -1);
 
             res.render('entertainment', {
                 title: 'Entertainment',
@@ -99,6 +80,7 @@ const task = cron.schedule('*/5 * * * *', async () =>  {
         await getAllPosts(conn, process.env.URL_FINANCE_NEWS, 'news');
         await getAllPosts(conn, process.env.URL_ENTERTAINMENT_NEWS, 'entertainment');
     } catch (err) {
+        task.destroy();
         console.error(`Error get new posts: ${ err.stack }`);
     }
 });
--- service/service.js
+++ service/service.js
@@ -4,7 +4,7 @@ const xml2js = require('xml2js');
 const download = require('./save.images');
 const pathImgNews = 'uploads/news';
 const pathImgEntertainment = 'uploads/entertainment';
-const parser = new xml2js.Parser({ attrkey: "ATTR" });
+const parser = new xml2js.Parser({ attrkey: 'ATTR' });
 
 const getPost = async (url, resource) => {
     try {
@@ -12,8 +12,8 @@ const getPost = async (url, resource) => {
         const html = await response.text();
         let file = [];
 
-        parser.parseString(html, function(error, result) {
-            if(error === null) {
+        parser.parseString(html, function (error, result) {
+            if (error === null) {
                 result.rss.channel[0].item.forEach(el => {
                     file.push({
                         image: el['media:content'] && el['media:content'][0] && el['media:content'][0].ATTR.url,
@@ -24,77 +24,84 @@ const getPost = async (url, resource) => {
                         description: el.description && el.description[0],
                         resource: resource,
                         publish_date: el.pubDate[0],
-                    })
-                })
-            }
-            else {
+                    });
+                });
+            } else {
                 console.log(error);
             }
         });
         return file;
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
-}
+};
 
 const getAllPosts = async (conn, resourceLink, resource) => {
     try {
         const result = await getPost(resourceLink, resource);
-        const query = `SELECT publish_date FROM posts WHERE resource = ${resource} ORDER BY publish_date;`;
+        //const query = "SELECT publish_date FROM posts WHERE resource = 'news' ORDER BY publish_date ASC LIMIT 1;";
+        const query = `SELECT publish_date FROM posts WHERE resource = '${ resource }' ORDER BY publish_date ASC LIMIT 1;`;
 
-        let newPost = [];
-        conn.query(query, function (error, data, fields) {
-            if (error) throw error;
 
-            if(data && result) {
-                newPost = data.filter(post => data < post.publish_date );
-            }
-            console.log('newPost', newPost);
-        });
+        (() => {
+            conn.query(query, async function (error, data, fields) {
+                if (error) throw error;
 
-        for await (let item of newPost.length ? newPost : result) {
-            let { image, publish_date } = item;
-            let pathImg = resource === 'news' ? pathImgNews : pathImgEntertainment;
-            let dateForImage = moment(publish_date).format('YYYY-MM-DD');
+                let newPost = [];
 
-            if(image) {
-                await download(image, pathImg, dateForImage, (msg) => {
-                    const post  = {
-                        title: item.title,
-                        link: item.link,
-                        source_link: item.source_link,
-                        source_name: item.source_name,
-                        description: item.description,
-                        resource: resource,
-                        image: msg,
-                        publish_date: moment(publish_date).format('YYYY-MM-DD, HH:mm:ss'),
-                    };
-                    const query = 'INSERT INTO posts SET ?';
-                    conn.query(query, post, function (error, results, fields) {
-                        if (error) throw error;
-                    });
-                    console.log('✅ Done!', msg);
-                });
-            } else {
-                const post  = {
-                    title: item.title,
-                    link: item.link,
-                    source_link: item.source_link,
-                    source_name: item.source_name,
-                    description: item.description,
-                    resource: resource,
-                    publish_date: moment(publish_date).format('YYYY-MM-DD, HH:mm:ss'),
-                };
-                const query = 'INSERT INTO posts SET ?';
-                conn.query(query, post, function (error, results, fields) {
-                    if (error) throw error;
-                });
-            }
-        }
+                if (data && result) {
+                    newPost = await result.filter(post => post.publish_date > data.publish_date);
+                }
+                console.log('newPost', newPost);
+                for await (let item of newPost.length ? newPost : result) {
+                    let { image, publish_date } = item;
+                    let pathImg = resource === 'news' ? pathImgNews : pathImgEntertainment;
+                    let dateForImage = moment(publish_date).format('YYYY-MM-DD');
+
+                    if (image) {
+                        await download(image, pathImg, dateForImage, (msg) => {
+                            const post = {
+                                title: item.title,
+                                link: item.link,
+                                source_link: item.source_link,
+                                source_name: item.source_name,
+                                description: item.description,
+                                resource: resource,
+                                image: msg,
+                                publish_date: moment(publish_date).format('YYYY-MM-DD, HH:mm:ss'),
+                            };
+                            const query = 'INSERT INTO posts SET ?';
+                            conn.query(query, post, function (error, results, fields) {
+                                if (error) throw error;
+                            });
+                            console.log('✅ Done!', msg);
+                        });
+                    } else {
+                        const post = {
+                            title: item.title,
+                            link: item.link,
+                            source_link: item.source_link,
+                            source_name: item.source_name,
+                            description: item.description,
+                            resource: resource,
+                            publish_date: moment(publish_date).format('YYYY-MM-DD, HH:mm:ss'),
+                        };
+                        const query = 'INSERT INTO posts SET ?';
+                        conn.query(query, post, function (error, results, fields) {
+                            if (error) throw error;
+                        });
+                    }
+                }
+
+
+            });
+        })();
+
+
         return result;
     } catch (e) {
         console.log(e);
     }
-}
+};
 
 module.exports = getAllPosts;
